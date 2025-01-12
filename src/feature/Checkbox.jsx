e35f2f7d4cf799c9c5814dc41abd74afb55c367f
Henry Morris <hrmorri5@ncsu.edu>
2025-01-12 13:51:16 -0500

    Fixed className bug

--- src/feature/Checkbox.jsx
+++ src/feature/Checkbox.jsx
@@ -5,7 +5,7 @@
 export default function Checkbox({ action }) {
     return(
         <>
-            <input type="checkbox" onChange={(e) => {action(e.target.checked)}} class="sr-only peer" />
+            <input type="checkbox" onChange={(e) => {action(e.target.checked)}} className="sr-only peer" />
             <span className="relative w-11 h-6 shrink-0 bg-neutral-300 dark:bg-neutral-500 rounded-full hover:cursor-pointer peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-neutral-500 dark:after:bg-neutral-300 after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-violet-300 peer-checked:after:bg-violet-500 dark:peer-checked:bg-violet-500 dark:peer-checked:after:bg-violet-300"></span>
         </>
     );
